cd83eecdf8da455280842f3da43ded7ce874b0d0
Bhavish Jan <bhavishjanparhyar@gmail.com>
2025-11-22 04:02:31 +0500

    Revert "animation fix"
    This reverts commit 2a52cb2a7ca0b54e2216920ea2dd618b1eb8b87f.

--- script.js
+++ script.js
@@ -55,15 +55,3 @@ document.addEventListener('DOMContentLoaded', () => {
     // Add Contact cards animation
     sr.reveal('.contact-card', { interval: 100 });
 });
-
-window.addEventListener('beforeunload', () => {
-  localStorage.setItem('scrollPosition', window.scrollY);
-});
-
-window.addEventListener('load', () => {
-  const scrollPosition = localStorage.getItem('scrollPosition');
-  if (scrollPosition) {
-    window.scrollTo(0, parseInt(scrollPosition));
-    localStorage.removeItem('scrollPosition');
-  }
-});
